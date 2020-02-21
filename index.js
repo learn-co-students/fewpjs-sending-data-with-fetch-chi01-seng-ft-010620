656e90495148ca24b106466c49b05e5c7b5d8e45
Zach Weber <weberzt@gmail.com>
2020-02-21 16:00:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+function configObj(name, email) {
+    return {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }) 
+    }
+};
+
+function submitData(name, email) {
+    fetch("http://localhost:3000/users", configObj(name, email) )
+        .then( resp => resp.json() )
+        // .then( userData => console.log(userData) )
+        .then( userData => {
+        document.body.innerHTML = userData["id"] 
+        })
+        .catch( error => document.body.innerHTML = error.message)
+};
