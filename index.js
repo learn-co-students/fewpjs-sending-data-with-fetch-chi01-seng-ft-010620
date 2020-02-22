c7403c55bfd77a68073deb02c6f947ee55e4d1f8
Cesar <cesarmota@ymail.com>
2020-02-21 22:58:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+function submitData(userName,email){
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            'Accept': "application/json"
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${email}`
+        })
+    }
+return fetch('http://localhost:3000/users', configObj)
+.then(resp => resp.json())
+//.then(data => console.log(data))
+//.then (data => renderPostData(data))
+.then( function(data){
+    document.body.innerHTML = data["id"]
+})
+.catch( function(error){
+ document.body.innerHTML= error.message
+})
+}
+
+function renderPostData(data){
+    const bodyTag = document.querySelector('body')
+  const elementtag = document.createElement("h1")
+
+    elementtag.innerHTML = `${data["id"]}`
+  bodyTag.append(elementtag)
+}
