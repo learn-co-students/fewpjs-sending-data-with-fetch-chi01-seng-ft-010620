6e77a70074c408086cb163069c758924b7e5c3d8
github username <github email address>
2020-02-23 17:07:28 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', newPostObj(name, email))
+        .then(resp => resp.json())
+        .then(object => document.body.innerHTML = object["id"])
+        .catch(err => document.body.innerHTML = err.message)
+}
+
+const newPostObj = (name, email) =>  {
+    return { 
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json", 
+        "Accept": "application/json"
+        }, 
+        body: JSON.stringify( {
+        name, 
+        email
+        })
+    }
+}
+
+// function submitData( name, email ) {
+//     return fetch( 'http://localhost:3000/users', {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           "Accept": "application/json"
+//         },
+//         body: JSON.stringify( {
+//           name,
+//           email
+//         } )
+//       } )
+//       .then( function ( response ) {
+//         return response.json()
+//       } )
+//       .then( function ( object ) {
+//         document.body.innerHTML = object[ "id" ]
+//       } )
+//       .catch( function ( error ) {
+//         document.body.innerHTML = error.message
+//       } )
+//   }
