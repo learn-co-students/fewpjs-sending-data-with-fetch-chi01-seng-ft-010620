ae929aef97fe400ec93d0f719d12bd6aa0d605af
github username <github email address>
2020-02-23 21:22:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+            name, 
+            email
+        })
+    })
+    .then( resp => resp.json() )
+    .then( newUser => document.body.innerHTML = (newUser.id) )
+    .catch( error => document.body.innerHTML = (error.message) )
+}
+   
